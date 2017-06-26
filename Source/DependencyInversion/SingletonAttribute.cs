53ea3a914cbffea74002dda3cc40354d51f335fc
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-26 20:18:08 +0200

    DependencyInversion in place

---
+++ Source/DependencyInversion/SingletonAttribute.cs
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+
+namespace doLittle.DependencyInversion
+{
+    /// <summary>
+    /// Indicates that a class is Singleton and should be treated as such
+    /// for any factory creating an instance of a class marked with this
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class SingletonAttribute : Attribute
+    {
+    }
+}
