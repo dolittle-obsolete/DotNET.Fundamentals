98d2ca0871966b7b42a5311d77d524feca223c4a
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-26 21:10:41 +0200

    Strings up and running

---
+++ Specifications/Strings/for_StringFormatBuilder/given/an_empty_string_format_builder.cs
@@ -0,0 +1,12 @@
+﻿using doLittle.Strings;
+using Machine.Specifications;
+
+namespace doLittle.Specs.Strings.for_StringFormatBuilder.given
+{
+    public class an_empty_string_format_builder
+    {
+        protected static StringFormatBuilder builder;
+
+        Establish context = () => builder = new StringFormatBuilder('.');
+    }
+}
---
+++ Specifications/Strings/for_StringFormatParser/given/a_string_format_parser.cs
@@ -0,0 +1,12 @@
+﻿using doLittle.Strings;
+using Machine.Specifications;
+
+namespace doLittle.Specs.Strings.for_StringFormatParser.given
+{
+    public class a_string_format_parser
+    {
+        protected static StringFormatParser parser;
+
+        Establish context = () => parser = new StringFormatParser();
+    }
+}
