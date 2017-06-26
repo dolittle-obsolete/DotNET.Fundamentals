262e224c1aa543359301a28a92d3691bb38a5b00
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-26 21:05:56 +0200

    Logging up and running

---
+++ Specifications/Logging/for_LogAppenders/given/no_log_appenders.cs
@@ -0,0 +1,12 @@
+﻿using doLittle.Logging;
+using Machine.Specifications;
+
+namespace doLittle.Specs.Logging.for_LogAppenders.given
+{
+    public class no_log_appenders : all_dependencies
+    {
+        protected static LogAppenders appenders;
+
+        Establish context = () => appenders = new LogAppenders(log_appenders_configurators.Object);
+    }
+}
