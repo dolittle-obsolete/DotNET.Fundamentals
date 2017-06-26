068ff9a58cf5c071b82cbca02b6afc6684546e39
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-26 21:05:22 +0200

    Moving SIngleton to new Execution project

--- Source/Execution/SingletonAttribute.cs
+++ Source/Execution/SingletonAttribute.cs
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 using System;
 
-namespace doLittle.DependencyInversion
+namespace doLittle.Execution
 {
     /// <summary>
     /// Indicates that a class is Singleton and should be treated as such
