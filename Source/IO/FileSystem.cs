9e82335815bbad521b9b13278f6d367306ea255d
Einar Ingebrigtsen <einar@dolittle.com>
2017-06-27 14:20:48 +0200

    Bringing in IO - FileSystem

---
+++ Source/IO/FileSystem.cs
@@ -0,0 +1,21 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System.Collections.Generic;
+using System.IO;
+
+namespace doLittle.IO
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IFileSystem"/>
+    /// </summary>
+    public class FileSystem : IFileSystem
+    {
+        /// <inheritdoc/>
+        public IEnumerable<FileInfo> GetFilesFrom(string path, string searchPattern)
+        {
+            return new DirectoryInfo(path).GetFiles(searchPattern);
+        }
+    }
+}
