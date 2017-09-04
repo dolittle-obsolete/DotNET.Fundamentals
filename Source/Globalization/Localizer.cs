b8a5170b7244d815a23335827da862323ac61271
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-04 21:27:10 +0200

    Moving over Globalization

---
+++ Source/Globalization/Localizer.cs
@@ -0,0 +1,33 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System.Globalization;
+
+namespace doLittle.Globalization
+{
+    /// <summary>
+    /// Represents a <see cref="ILocalizer"/>
+    /// </summary>
+    public class Localizer : ILocalizer
+    {
+        /// <inheritdoc/>
+        public LocalizationScope BeginScope()
+        {
+            var scope = LocalizationScope.FromCurrentThread();
+
+            // TODO: We need to be able to configure culture for the application
+            //CultureInfo.CurrentCulture = Configure.Instance.Culture;
+            //CultureInfo.CurrentUICulture = Configure.Instance.UICulture;
+
+            return scope;
+        }
+
+        /// <inheritdoc/>
+        public void EndScope(LocalizationScope scope)
+        {
+            CultureInfo.CurrentCulture = scope.Culture;
+            CultureInfo.CurrentUICulture = scope.UICulture;
+        }
+    }
+}
---
+++ Specifications/Globalization/given/a_localizer_mock.cs
@@ -0,0 +1,17 @@
+﻿using doLittle.Globalization;
+using Machine.Specifications;
+using Moq;
+
+namespace doLittle.Specs.Globalization.given
+{
+    public class a_localizer_mock
+    {
+        protected static Mock<ILocalizer> localizer_mock;
+
+        Establish context = () =>
+                                {
+                                    localizer_mock = new Mock<ILocalizer>();
+                                    localizer_mock.Setup(l => l.BeginScope()).Returns(LocalizationScope.FromCurrentThread);
+                                };
+    }
+}
