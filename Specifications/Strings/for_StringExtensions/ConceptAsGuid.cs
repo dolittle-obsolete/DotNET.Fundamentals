b801d223b6021ac90682645dad00b05d2057b1ce
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-04 21:29:56 +0200

    Specs for StringExtensions

---
+++ Specifications/Strings/for_StringExtensions/ConceptAsGuid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using doLittle.Concepts;
+
+namespace doLittle.Specs.Extensions.for_StringExtensions
+{
+    public class ConceptAsGuid : ConceptAs<Guid>
+    {
+        public static implicit operator ConceptAsGuid(Guid guid)
+        {
+            return new ConceptAsGuid() { Value = guid };
+        }
+    }
+}
