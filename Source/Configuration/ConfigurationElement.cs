10328a0696fa1b489db7d853e6e84abf14cb122e
Einar Ingebrigtsen <einar@dolittle.com>
2017-10-08 00:38:26 +0200

    Started work on Configuration

---
+++ Source/Configuration/ConfigurationElement.cs
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2008-2017 doLittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using doLittle.Concepts;
+
+namespace Configuration
+{
+    /// <summary>
+    /// Represents a configuration element
+    /// </summary>
+    public class ConfigurationElement<T> : ConceptAs<T>
+    {
+        
+    }
+}
