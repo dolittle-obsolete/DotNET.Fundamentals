7bc29142e9518e0ed7f8c9cd7aa066f1c83b4856
Einar Ingebrigtsen <einar@dolittle.com>
2017-10-12 12:55:34 +0200

    Improved build pipeline for VSCode

--- find.js
+++ find.js
@@ -13,9 +13,22 @@ let rerun = false;
 let type = "default";
 let rootDir = "";
 let specsFor = false;
+let forTests = false;
+let ignoreRewriteFolder = false;
 
 let settings = {
 };
+
+
+
+let logFile = "/Users/einari/log";
+//fs.writeFileSync(logFile, "");
+
+function log(message) {
+    fs.appendFileSync(logFile, message + "\n");
+}
+
+log("Current Folder : " + currentDir);
 
 process.argv.forEach((item, index) => {
     if (index == 0) return;
@@ -25,12 +38,16 @@ process.argv.forEach((item, index) => {
     }
     if (item.indexOf("/type:") == 0) {
         type = item.substr(6);
-    } 
+        log("Type : " + type);
+    }
     if (item.indexOf("/rerun") == 0) {
         rerun = true;
-    } 
-    if( item.indexOf("/specsFor") == 0 ) {
-        specsFor = true;
+    }
+    if (item.indexOf("/ignoreRewriteFolder") == 0) {
+        ignoreRewriteFolder = true;
+    }
+    if (item.indexOf("/forTests") == 0) {
+        forTests = true;
     }
 });
 
@@ -38,6 +55,20 @@ if (rootDir.length == 0) {
     console.log("Missing rootDir - use /root:<root dir> - the rootdir is where any output files are stored");
     return;
 }
+
+
+if (type == "build" && forTests) {
+    currentDir = currentDir.replace("Source", "Specifications");
+} else {
+    if (!ignoreRewriteFolder) {
+        if (type == "test" && currentDir.indexOf("Source") >= 0) specsFor = true;
+        if (type == "build" && currentDir.indexOf("Specifications") >= 0) currentDir = currentDir.replace("Specifications", "Source");
+    }
+}
+
+
+
+log("Folder to use : " + currentDir);
 
 let settingsFile = path.join(rootDir, ".buildsettings");
 if (fs.existsSync(settingsFile)) {
@@ -50,15 +81,17 @@ let directoryToRun = "";
 if (rerun) {
     directoryToRun = settings[type].workingDir;
 } else {
+    let found = false;
     while (currentDir.length > 0) {
         try {
             let content = fs.readdirSync(currentDir);
             let files = content.filter(function (elm) { return elm.match(/.*\.(csproj|sln)/ig); });
             if (files.length == 1) {
                 settings[type] = {
-                    workingDir:currentDir
+                    workingDir: currentDir
                 };
-                
+                found = true;
+
                 break;
             }
         } catch (ex) { }
@@ -66,14 +99,17 @@ if (rerun) {
         currentDir = currentDir.substr(0, currentDir.lastIndexOf(path.sep));
     }
 
-    directoryToRun = currentDir;
+    if (found) directoryToRun = currentDir;
+    else directoryToRun = settings[type].workingDir;
 }
 
 fs.writeFileSync(settingsFile, JSON.stringify(settings), "utf8");
 
-if( specsFor == true ) {
+if (specsFor == true) {
     directoryToRun = directoryToRun.replace("Source", "Specifications");
 }
+log("Directory to run : " + directoryToRun);
+
 
 console.log(directoryToRun);
 
