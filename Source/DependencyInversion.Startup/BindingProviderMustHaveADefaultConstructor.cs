c8b5b4887bd6b2ea7dc8e662e06d180baaa50e0b
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-16 15:22:57 +0100

    Adding exception for when a Provider does not have a default constructor

---
+++ Source/DependencyInversion.Startup/BindingProviderMustHaveADefaultConstructor.cs
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+
+namespace doLittle.DependencyInversion.Startup
+{
+    /// <summary>
+    /// The exception that gets thrown if a binding provider does not have a default constructor
+    /// </summary>
+    public class BindingProviderMustHaveADefaultConstructor : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="BindingProviderMustHaveADefaultConstructor"/>
+        /// </summary>
+        /// <param name="providerType"><see cref="Type"/> of provider</param>
+        public BindingProviderMustHaveADefaultConstructor(Type providerType) : base($"{providerType.AssemblyQualifiedName} does not have a default constructor")
+        {
+        }
+    }
+}
