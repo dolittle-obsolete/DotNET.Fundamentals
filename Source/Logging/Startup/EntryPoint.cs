95d78177db6ed66b282ab0a6bbcc37d405e1a94e
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-16 19:57:54 +0100

    Moving startup into EntryPoint for Logging

---
+++ Source/Logging/Startup/EntryPoint.cs
@@ -0,0 +1,51 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using doLittle.Collections;
+using doLittle.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace doLittle.Logging.Startup
+{
+    /// <summary>
+    /// Represents the entrypoint for initializating Logging
+    /// </summary>
+    public class EntryPoint
+    {
+        /// <summary>
+        /// Discover any <see cref="ICanConfigureLogAppenders"/> from the entry assembly and setup 
+        /// <see cref="ILogAppenders"/>
+        /// </summary>
+        /// <returns>An instance of <see cref="ILogAppenders"/> that can be used</returns>
+        public static ILogAppenders Initialize(ILoggerFactory loggerFactory)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+            var types = assembly.GetTypes();
+
+            var configuratorTypes = types.Where(t => t.HasInterface<ICanConfigureLogAppenders>());
+
+            var configurators = new List<ICanConfigureLogAppenders>();
+            configurators.Add(new DefaultLogAppendersConfigurator(loggerFactory));
+            configuratorTypes.ForEach(c =>
+            {
+                ThrowIfLogAppenderConfiguratorIsMissingDefaultConstructor(c);
+                var configurator = Activator.CreateInstance(c) as ICanConfigureLogAppenders;
+                configurators.Add(configurator);
+            });
+
+            var logAppenders = new LogAppenders(configurators);
+            return logAppenders;
+        }
+
+        static void ThrowIfLogAppenderConfiguratorIsMissingDefaultConstructor(Type c)
+        {
+            if (!c.HasDefaultConstructor()) throw new LogAppenderConfiguratorMissingDefaultConstructor(c);
+        }
+    }
+}
