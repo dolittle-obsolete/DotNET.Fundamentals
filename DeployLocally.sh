9d309fadaaa2310c8c07d7c7d592be7cabd3e7b5
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-16 15:21:11 +0100

    Making it smoother to deploy nugets locally

---
+++ DeployLocally.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PACKAGEDIR=$PWD/../Packages
+
+find ~/.nuget/packages/ -name 2.0.0-alpha2.1000 -exec rm -rf {} \;
+
+if [ ! -d "$PACKAGEDIR" ]; then
+    mkdir $PACKAGEDIR
+fi
+
+rm $PACKAGEDIR/*
+dotnet pack -p:PackageVersion=2.0.0-alpha2.1000 --include-symbols --include-source -o $PACKAGEDIR
