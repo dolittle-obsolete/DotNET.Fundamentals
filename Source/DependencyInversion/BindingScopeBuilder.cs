a6038ef6264344d310bea831211646385a4d8d16
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-17 18:12:10 +0100

    Moving SIngletonAttribute back to Execution

--- Source/DependencyInversion/BindingScopeBuilder.cs
+++ Source/DependencyInversion/BindingScopeBuilder.cs
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 using System.Linq;
 using System.Reflection;
+using doLittle.Execution;
 using doLittle.Reflection;
 
 namespace doLittle.DependencyInversion
