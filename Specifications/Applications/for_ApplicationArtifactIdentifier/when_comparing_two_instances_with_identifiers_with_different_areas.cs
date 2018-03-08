559bcc01ccf55c99758b2400387ca911d2f6863e
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-08 15:20:24 +0100

    Adding copyright header

--- Specifications/Applications/for_ApplicationArtifactIdentifier/when_comparing_two_instances_with_identifiers_with_different_areas.cs
+++ Specifications/Applications/for_ApplicationArtifactIdentifier/when_comparing_two_instances_with_identifiers_with_different_areas.cs
@@ -1,4 +1,8 @@
-﻿using Machine.Specifications;
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using Machine.Specifications;
 using It = Machine.Specifications.It;
 
 namespace Dolittle.Applications.Specs.for_ApplicationArtifactIdentifier
