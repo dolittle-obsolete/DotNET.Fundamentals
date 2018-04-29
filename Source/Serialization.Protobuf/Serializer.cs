c233cf54e0b95e0e8a27f115c37cda7ec8d34674
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-29 23:06:17 +0200

    Making it slightly more readable

--- Source/Serialization.Protobuf/Serializer.cs
+++ Source/Serialization.Protobuf/Serializer.cs
@@ -52,71 +52,19 @@ namespace Dolittle.Serialization.Protobuf
 
                     IValueConverter converter = null;
 
-                    if( _valueConverters.CanConvert(type) )
+                    if (_valueConverters.CanConvert(type))
                     {
                         converter = _valueConverters.GetConverterFor(type);
                         type = converter.SerializedAs;
 
-                    } else if ( type.IsConcept())
+                    }
+                    else if (type.IsConcept())
                     {
                         type = type.GetConceptValueType();
                     }
 
-                    if (type == typeof(Guid))
-                    {
-                        var length = inputStream.ReadLength();
-                        var guidAsBytes = inputStream.ReadBytes();
-                        value = new Guid(guidAsBytes.ToByteArray());
-                    }
-                    else if (type == typeof(string))
-                    {
-                        var length = inputStream.ReadLength();
-                        value = inputStream.ReadString();
-                    }
-                    else if (type == typeof(int))
-                    {
-                        value = inputStream.ReadInt32();
-                    }
-                    else if (type == typeof(Int64))
-                    {
-                        value = inputStream.ReadInt64();
-                    }
-                    else if (type == typeof(uint))
-                    {
-                        value = inputStream.ReadUInt32();
-                    }
-                    else if (type == typeof(uint))
-                    {
-                        value = inputStream.ReadInt64();
-                    }
-                    else if (type == typeof(float))
-                    {
-                        value = inputStream.ReadFloat();
-                    }
-                    else if (type == typeof(double))
-                    {
-                        value = inputStream.ReadDouble();
-                    }
-                    else if (type == typeof(bool))
-                    {
-                        value = inputStream.ReadBool();
-                    }
-                    else if (type == typeof(DateTimeOffset))
-                    {
-                        value = DateTimeOffset.FromUnixTimeMilliseconds(inputStream.ReadInt64());
-                    }
-                    else if (type == typeof(DateTime))
-                    {
-                        value = DateTime.FromFileTimeUtc(inputStream.ReadInt64());
-                    }
-
-                    if( converter != null ) value = converter.ConvertFrom(value);
-
+                    value = ReadValue(inputStream, value, type, converter);
                     propertyDescription.Property.SetValue(instance, value);
-                }
-                else
-                {
-
                 }
 
                 tag = inputStream.ReadTag();
@@ -124,6 +72,7 @@ namespace Dolittle.Serialization.Protobuf
 
             return instance;
         }
+
 
         /// <inheritdoc/>
         public T FromProtobuf<T>(byte[] bytes, bool includesLength = false)
@@ -152,78 +101,22 @@ namespace Dolittle.Serialization.Protobuf
                 var number = property.Number;
                 var value = property.Property.GetValue(instance);
 
-                if( _valueConverters.CanConvert(type) )
+                if (_valueConverters.CanConvert(type))
                 {
                     var converter = _valueConverters.GetConverterFor(type);
                     type = converter.SerializedAs;
                     value = converter.ConvertTo(value);
-                } else if (type.IsConcept())
+                }
+                else if (type.IsConcept())
                 {
                     type = type.GetConceptValueType();
                     value = value.GetConceptValue();
                 }
 
-                if (type == typeof(Guid))
-                {
-                    outputStream.WriteTag(number, WireType.LengthDelimited);
-                    var guidAsBytes = ((Guid) value).ToByteArray();
-                    outputStream.WriteLength(guidAsBytes.Length);
-                    outputStream.WriteBytes(ByteString.CopyFrom(guidAsBytes));
-                }
-                else if (type == typeof(string))
-                {
-                    var valueAsString = value as string;
-                    outputStream.WriteTag(number, WireType.LengthDelimited);
-                    outputStream.WriteLength(UTF8Encoding.UTF8.GetByteCount(valueAsString));
-                    outputStream.WriteString(valueAsString);
-                }
-                else if (type == typeof(int))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteInt32((int) value);
-                }
-                if (type == typeof(Int64))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteInt64((Int64) value);
-                }
-                else if (type == typeof(uint))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteUInt32((uint) value);
-                }
-                if (type == typeof(UInt64))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteUInt64((UInt64) value);
-                }
-                else if (type == typeof(float))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteFloat((float) value);
-                }
-                else if (type == typeof(double))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteDouble((double) value);
-                }
-                else if (type == typeof(bool))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteBool((bool) value);
-                }
-                else if (type == typeof(DateTimeOffset))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteInt64(((DateTimeOffset) value).ToUnixTimeMilliseconds());
-                }
-                else if (type == typeof(DateTime))
-                {
-                    outputStream.WriteTag(number, WireType.Varint);
-                    outputStream.WriteInt64(((DateTime) value).ToFileTimeUtc());
-                }
+                WriteValue(outputStream, type, number, value);
             });
         }
+
 
         /// <inheritdoc/>
         public byte[] ToProtobuf<T>(T instance, bool includeLength = false)
@@ -234,6 +127,125 @@ namespace Dolittle.Serialization.Protobuf
                 return stream.ToArray();
             }
         }
+
+        object ReadValue(CodedInputStream inputStream, object value, Type type, IValueConverter converter)
+        {
+            if (type == typeof(Guid))
+            {
+                var length = inputStream.ReadLength();
+                var guidAsBytes = inputStream.ReadBytes();
+                value = new Guid(guidAsBytes.ToByteArray());
+            }
+            else if (type == typeof(string))
+            {
+                var length = inputStream.ReadLength();
+                value = inputStream.ReadString();
+            }
+            else if (type == typeof(int))
+            {
+                value = inputStream.ReadInt32();
+            }
+            else if (type == typeof(Int64))
+            {
+                value = inputStream.ReadInt64();
+            }
+            else if (type == typeof(uint))
+            {
+                value = inputStream.ReadUInt32();
+            }
+            else if (type == typeof(uint))
+            {
+                value = inputStream.ReadInt64();
+            }
+            else if (type == typeof(float))
+            {
+                value = inputStream.ReadFloat();
+            }
+            else if (type == typeof(double))
+            {
+                value = inputStream.ReadDouble();
+            }
+            else if (type == typeof(bool))
+            {
+                value = inputStream.ReadBool();
+            }
+            else if (type == typeof(DateTimeOffset))
+            {
+                value = DateTimeOffset.FromUnixTimeMilliseconds(inputStream.ReadInt64());
+            }
+            else if (type == typeof(DateTime))
+            {
+                value = DateTime.FromFileTimeUtc(inputStream.ReadInt64());
+            }
+
+            if (converter != null) value = converter.ConvertFrom(value);
+            return value;
+        }
+
+        void WriteValue(CodedOutputStream outputStream, Type type, int number, object value)
+        {
+            if (type == typeof(Guid))
+            {
+                outputStream.WriteTag(number, WireType.LengthDelimited);
+                var guidAsBytes = ((Guid)value).ToByteArray();
+                outputStream.WriteLength(guidAsBytes.Length);
+                outputStream.WriteBytes(ByteString.CopyFrom(guidAsBytes));
+            }
+            else if (type == typeof(string))
+            {
+                var valueAsString = value as string;
+                outputStream.WriteTag(number, WireType.LengthDelimited);
+                outputStream.WriteLength(UTF8Encoding.UTF8.GetByteCount(valueAsString));
+                outputStream.WriteString(valueAsString);
+            }
+            else if (type == typeof(int))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteInt32((int)value);
+            }
+            if (type == typeof(Int64))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteInt64((Int64)value);
+            }
+            else if (type == typeof(uint))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteUInt32((uint)value);
+            }
+            if (type == typeof(UInt64))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteUInt64((UInt64)value);
+            }
+            else if (type == typeof(float))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteFloat((float)value);
+            }
+            else if (type == typeof(double))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteDouble((double)value);
+            }
+            else if (type == typeof(bool))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteBool((bool)value);
+            }
+            else if (type == typeof(DateTimeOffset))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteInt64(((DateTimeOffset)value).ToUnixTimeMilliseconds());
+            }
+            else if (type == typeof(DateTime))
+            {
+                outputStream.WriteTag(number, WireType.Varint);
+                outputStream.WriteInt64(((DateTime)value).ToFileTimeUtc());
+            }
+        }
+        
+        
 
         int GetSizeFor<T>(T instance, MessageDescription messageDescription)
         {
