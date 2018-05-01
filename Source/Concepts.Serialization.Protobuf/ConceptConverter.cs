43f521244047689c8dac42a72b46546e994ccdf7
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-01 22:42:11 +0200

    Introducing own project for Concept serialization for Protobuf

---
+++ Source/Concepts.Serialization.Protobuf/ConceptConverter.cs
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Dolittle.Serialization.Protobuf;
+
+namespace Dolittle.Concepts.Serialization.Protobuf
+{
+    /// <summary>
+    /// Represents an implementation of <see cref="IValueConverter"/> that deals with serializing and deserialing of <see cref="ConceptAs{T}"/>
+    /// </summary>
+    public class ConceptConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public bool CanConvert(Type objectType)
+        {
+            return objectType.IsConcept();
+        }
+
+        /// <inheritdoc/>
+        public Type SerializedAs(Type objectType)
+        {
+            return objectType.GetConceptValueType();
+        }
+
+        /// <inheritdoc/>
+        public object ConvertTo(object value)
+        {
+            return value.GetConceptValue();
+        }
+
+        /// <inheritdoc/>
+        public object ConvertFrom(Type objectType, object value)
+        {
+            return ConceptFactory.CreateConceptInstance(objectType, value);
+        }            
+    }
+}
