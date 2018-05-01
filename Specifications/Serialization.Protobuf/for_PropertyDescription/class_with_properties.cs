47a19469c1277824dbe7c40aeee66b6061c4b869
Einar Ingebrigtsen <einar@dolittle.com>
2018-05-01 10:28:47 +0200

    Added specs to verify hash generation of property name

---
+++ Specifications/Serialization.Protobuf/for_PropertyDescription/class_with_properties.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace Dolittle.Serialization.Protobuf.for_PropertyDescription
+{
+    public class class_with_properties
+    {
+        public static PropertyInfo first_property = typeof(class_with_properties).GetProperty("FirstProperty");
+        public static PropertyInfo second_property = typeof(class_with_properties).GetProperty("SecondProperty");
+        public int FirstProperty {  get; set; }
+        public string SecondProperty {  get; set; }
+    }
+}
