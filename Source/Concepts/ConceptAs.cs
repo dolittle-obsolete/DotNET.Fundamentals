72071d2bd1edae50c4ef348476c3c0588f3f9e27
Michael Smith <michael@dolittle.com>
2018-06-14 15:29:06 +0200

    Implement <= and >= for ConceptAs

--- Source/Concepts/ConceptAs.cs
+++ Source/Concepts/ConceptAs.cs
@@ -79,6 +79,16 @@ namespace Dolittle.Concepts
             return a.CompareTo(b) == -1;
         }
 
+        public static bool operator >=(ConceptAs<T> a, ConceptAs<T> b)
+        {
+            return a.CompareTo(b) > -1;
+        }
+
+        public static bool operator <= (ConceptAs<T> a, ConceptAs<T> b)
+        {
+            return a.CompareTo(b) < 1;
+        }
+        
         public override int GetHashCode()
         {
             return HashCodeHelper.Generate(typeof (T), Value);
