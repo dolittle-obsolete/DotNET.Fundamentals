67af175dcd05b7c562f0506322b9042271aa997c
Sindre Wilting <sindrewilting@gmail.com>
2018-06-28 20:17:46 +0200

    Change spec It name

--- Specifications/Applications/for_ApplicationArtifactIdentifier/when_comparing_two_instances_with_artifact_of_same_generation.cs
+++ Specifications/Applications/for_ApplicationArtifactIdentifier/when_comparing_two_instances_with_artifact_of_same_generation.cs
@@ -10,6 +10,6 @@ namespace Dolittle.Applications.Specs.for_ApplicationArtifactIdentifier
 
         Because of = () => result = identifier_a == identifier_b;
 
-        It should_not_be_considered_the_same = () => result.ShouldBeTrue();
+        It should_be_considered_the_same = () => result.ShouldBeTrue();
     }
 }
