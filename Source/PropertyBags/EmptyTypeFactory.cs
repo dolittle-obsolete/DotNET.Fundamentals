e4d689048fb9b277f0361665c57b7fad12bd2276
Michael Smith <michael@dolittle.com>
2018-08-22 10:26:26 +0200

    Factory that can create an immutable empty class (no properties, no non-default ctor)

---
+++ Source/PropertyBags/EmptyTypeFactory.cs
@@ -0,0 +1,43 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace Dolittle.PropertyBags
+{
+    using System;
+    using System.Collections.Concurrent;
+    using Dolittle.Execution;
+    using Dolittle.Reflection;
+
+    /// <summary>
+    /// Creates a instance of the immutable type using the default ctor
+    /// </summary>
+    [Singleton]
+    public class EmptyTypeFactory : ITypeFactory
+    {
+        /// <inheritdoc />
+        public object Build(Type type, IObjectFactory objectFactory, PropertyBag source)
+        {
+           return Activator.CreateInstance(type);
+        }
+
+        /// <inheritdoc />
+        public T Build<T>(IObjectFactory objectFactory, PropertyBag source)
+        {
+            return (T)Build(typeof(T),objectFactory,source);
+        }
+
+        /// <inheritdoc />
+        public bool CanBuild(Type type)
+        {
+            return !type.HasNonDefaultConstructor() && type.HasDefaultConstructor() && !type.HasVisibleProperties();
+        }
+
+        /// <inheritdoc />
+        public bool CanBuild<T>()
+        {
+            return CanBuild(typeof(T));
+        }
+    }
+}
---
+++ Specifications/PropertyBags/for_ObjectFactory/when_building/a_type_with_no_properties/with_no_ctor.cs
@@ -0,0 +1,31 @@
+﻿namespace Dolittle.PropertyBags.Specs.for_ObjectFactory.when_building.an_immutable_with_concepts
+{
+    using Machine.Specifications;
+    using Dolittle.PropertyBags;
+    using Dolittle.PropertyBags.Specs;
+    using System;
+
+    [Subject(typeof(ObjectFactory), "Build")]
+    public class with_no_ctor : given.an_object_factory
+    {
+        static IObjectFactory factory;
+        static EmptyClass empty;
+        static PropertyBag source;
+        static object result;
+        Establish context = () => 
+        {
+            factory = instance;
+            empty = new EmptyClass();
+            source = empty.ToPropertyBag();
+        };
+
+        Because of = () => result = factory.Build(typeof(EmptyClass), source);
+
+        It should_build_an_instance_of_the_type = () => result.ShouldBeOfExactType<EmptyClass>();
+    }
+
+    public class EmptyClass 
+    {
+
+    }
+}
