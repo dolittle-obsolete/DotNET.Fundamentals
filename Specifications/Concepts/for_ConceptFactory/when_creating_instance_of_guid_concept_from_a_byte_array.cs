e626836a9e8580269c0666bad765807fe048fe3e
Michael Smith <michael@dolittle.com>
2018-08-28 19:06:45 +0200

    Fix recursive error on creation of CorrelationId

--- Specifications/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_from_a_byte_array.cs
+++ Specifications/Concepts/for_ConceptFactory/when_creating_instance_of_guid_concept_from_a_byte_array.cs
@@ -1,11 +1,12 @@
-﻿using Dolittle.Concepts;
+using Dolittle.Concepts;
 using Machine.Specifications;
 using System;
 
 namespace Dolittle.Specs.Concepts.for_ConceptFactory
 {
+
     [Subject(typeof(ConceptFactory))]
-    public class when_creating_instance_of_guid_concept
+    public class when_creating_instance_of_guid_concept_from_a_byte_array
     {
         const string  guid_value_as_string = "4AB92720-3138-4A7B-A7E9-2A49F6624736";
         static Guid guid;
@@ -13,8 +14,8 @@ namespace Dolittle.Specs.Concepts.for_ConceptFactory
 
         Establish context = () => guid = Guid.Parse(guid_value_as_string);
 
-        Because of = () => result = ConceptFactory.CreateConceptInstance(typeof(GuidConcept), guid) as GuidConcept;
+        Because of = () => result = ConceptFactory.CreateConceptInstance(typeof(GuidConcept), guid.ToByteArray()) as GuidConcept;
 
         It should_hold_the_correct_guid_value = () => result.Value.ToString().ToUpper().ShouldEqual(guid_value_as_string);
-    } 
+    }
 }
