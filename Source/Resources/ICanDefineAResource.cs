b794079870a58f5c9dc0e459688057fe05609708
Einar Ingebrigtsen <einar@dolittle.com>
2018-08-31 15:42:32 +0200

    Xml comments

--- Source/Resources/ICanDefineAResource.cs
+++ Source/Resources/ICanDefineAResource.cs
@@ -8,23 +8,22 @@ using System.Collections.Generic;
 namespace Dolittle.Resources
 {
     /// <summary>
-    /// 
+    /// Defines a system that defines a resource
     /// </summary>
     public interface ICanDefineAResource
     {
         /// <summary>
-        /// 
+        /// Gets the <see cref="ResourceType"/> it represents
         /// </summary>
-        /// <value></value>
         ResourceType    ResourceType { get; }
 
         /// <summary>
-        /// 
+        /// Gets the <see cref="ResourceTypeName"/> of the resource - identified typically in configuration files
         /// </summary>
         ResourceTypeName ResourceTypeName { get; }
 
         /// <summary>
-        /// 
+        /// Gets the <see cref="Type"/> of the configuration object to expect and create proper bindings for
         /// </summary>
         Type ConfigurationObjectType { get; }
     }
