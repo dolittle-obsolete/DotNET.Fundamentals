aefd750d094747b2862931f5dc061686bf156850
Sindre Wilting <sindrewilting@gmail.com>
2018-10-26 09:27:40 +0200

    Specs

---
+++ Specifications/Tenancy.Configuration/given/all.cs
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Dolittle.Tenancy.Configuration.Specs.given
+{
+    public class all
+    {
+        protected static readonly TenantStrategy first_strategy = "first_strategy";
+        protected static readonly TenantStrategy second_strategy = "second_strategy";
+    }
+}
