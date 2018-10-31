69e4613c3976aa5cc286607354cf552b16a5be5a
Sindre Wilting <sindrewilting@gmail.com>
2018-10-31 21:02:58 +0100

    Suport netstandard 2 only

--- Source/Logging/DefaultLogAppendersConfigurator.cs
+++ Source/Logging/DefaultLogAppendersConfigurator.cs
@@ -2,10 +2,7 @@
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
-#if(!NET461)
 using Microsoft.Extensions.Logging;
-#endif
-
 namespace Dolittle.Logging
 {
     /// <summary>
@@ -14,7 +11,6 @@ namespace Dolittle.Logging
     public class DefaultLogAppendersConfigurator : ICanConfigureLogAppenders
     {
         GetCurrentLoggingContext _getCurrentLoggingContext;
-#if(!NET461)
         ILoggerFactory _loggerFactory;
         /// <summary>
         /// Initializes a new instance of <see cref="DefaultLogAppendersConfigurator"/>
@@ -26,26 +22,12 @@ namespace Dolittle.Logging
             _loggerFactory = loggerFactory;
             _getCurrentLoggingContext = getCurrentLoggingContext;
         }
-#else
-        /// <summary>
-        /// Initializes a new instance of <see cref="DefaultLogAppendersConfigurator"/>
-        /// </summary>
-        /// <param name="getCurrentLoggingContext"></param>
-        public DefaultLogAppendersConfigurator(GetCurrentLoggingContext getCurrentLoggingContext)
-        {
-            _getCurrentLoggingContext = getCurrentLoggingContext;
-        }
-#endif
-
 
         /// <inheritdoc/>
         public void Configure(ILogAppenders appenders)
         {
-#if (NET461)
-            var defaultLogAppender = new DefaultLogAppender(_getCurrentLoggingContext);
-#else
             var defaultLogAppender = new DefaultLogAppender(_getCurrentLoggingContext, _loggerFactory);
-#endif
+
             appenders.Add(defaultLogAppender);
             appenders.Add(new Json.JsonLogAppender(_getCurrentLoggingContext));
         }
