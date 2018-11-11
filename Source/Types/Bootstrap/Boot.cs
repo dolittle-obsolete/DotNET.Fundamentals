cabdbb0402088aa22da408de4acfa5511b1a87ca
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-11 08:51:53 +0100

    Renaming for consistency

--- Source/Types/Bootstrap/Boot.cs
+++ Source/Types/Bootstrap/Boot.cs
@@ -10,18 +10,19 @@ namespace Dolittle.Types.Bootstrap
     /// <summary>
     /// Represents the entrypoint for starting up and initialization for an app using the Type system
     /// </summary>
-    public class EntryPoint
+    public class Boot
     {
         /// <summary>
         /// Initialize systems needed for the type system and discovery mechanisms to work
         /// </summary>
         /// <param name="assemblies"><see cref="IAssemblies"/> that will be used</param>
+        /// <param name="entryAssembly"><see cref="Assembly"/> to use as entry assembly - null indicates it will ask for the entry assembly</param>
         /// <returns><see cref="ITypeFinder"/> that can be used</returns>
-        public static ITypeFinder Initialize(IAssemblies assemblies)
+        public static ITypeFinder Start(IAssemblies assemblies, Assembly entryAssembly = null)
         {
             var contractToImplementorsMap = new ContractToImplementorsMap();
 
-            var entryAssembly = Assembly.GetEntryAssembly();
+            if( entryAssembly == null ) entryAssembly = Assembly.GetEntryAssembly();
             contractToImplementorsMap.Feed(entryAssembly.GetTypes());
             
             var typeFinder = new TypeFinder(assemblies, contractToImplementorsMap);
