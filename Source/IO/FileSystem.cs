81ba101b429daead0545fd49cd770cbe5f8d3762
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-21 14:46:02 +0100

    Adding basic functionality

--- Source/IO/FileSystem.cs
+++ Source/IO/FileSystem.cs
@@ -13,9 +13,27 @@ namespace Dolittle.IO
     public class FileSystem : IFileSystem
     {
         /// <inheritdoc/>
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        /// <inheritdoc/>
+        public string GetCurrentDirectory()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        /// <inheritdoc/>
         public IEnumerable<FileInfo> GetFilesFrom(string path, string searchPattern)
         {
             return new DirectoryInfo(path).GetFiles(searchPattern);
+        }
+
+        /// <inheritdoc/>
+        public string ReadAllText(string filename)
+        {
+            return File.ReadAllText(filename);
         }
     }
 }
