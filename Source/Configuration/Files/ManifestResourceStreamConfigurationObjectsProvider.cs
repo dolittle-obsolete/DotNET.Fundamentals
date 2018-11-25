16620ce62ea4fc24e7e301425485bb901496e398
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-25 18:37:37 +0100

    Basic implementation for getting configuration from embedded resources

--- Source/Configuration/Files/ManifestResourceStreamConfigurationObjectsProvider.cs
+++ Source/Configuration/Files/ManifestResourceStreamConfigurationObjectsProvider.cs
@@ -3,24 +3,67 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Dolittle.Assemblies;
 
 namespace Dolittle.Configuration.Files
 {
     /// <summary>
-    /// 
+    /// Represents a <see cref="ICanProvideConfigurationObjects">configuration provider</see> for 
+    /// embedded resources.
     /// </summary>
+    /// <remarks>
+    /// It will only look for embedded resources in the entry assembly
+    /// </remarks>
     public class ManifestResourceStreamConfigurationObjectsProvider : ICanProvideConfigurationObjects
     {
+        readonly Assembly _entryAssembly;
+        readonly IConfigurationFileParsers _parsers;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ManifestResourceStreamConfigurationObjectsProvider"/>
+        /// </summary>
+        /// <param name="assemblies"><see cref="IAssemblies"/> for getting assembly for embedded resources</param>
+        /// <param name="parsers"><see cref="IConfigurationFileParsers"/> for parsing</param>
+        public ManifestResourceStreamConfigurationObjectsProvider(
+            IAssemblies assemblies,
+            IConfigurationFileParsers parsers)
+        {
+            _entryAssembly = assemblies.EntryAssembly;
+            _parsers = parsers;
+        }
+
         /// <inheritdoc/>
         public bool CanProvide(Type type)
         {
-            return false;
+            var filename = GetFilenameFor(type);
+            var resourceNames = _entryAssembly.GetManifestResourceNames().Where(_ => _.EndsWith(filename)).ToArray();
+            if( resourceNames.Length > 1 ) throw new MultipleFilesAvailableOfSameType(type, resourceNames);
+            return resourceNames.Length == 1;
         }
 
         /// <inheritdoc/>
         public object Provide(Type type)
         {
-            return null;
+            var filename = GetFilenameFor(type);
+            var resourceName = _entryAssembly.GetManifestResourceNames().Where(_ => _.EndsWith(filename)).SingleOrDefault();
+            if( resourceName != null )
+            {
+                using( var reader = new StreamReader(_entryAssembly.GetManifestResourceStream(resourceName)) )
+                {
+                    var content = reader.ReadToEnd();
+                    return _parsers.Parse(type, resourceName, content);
+                }
+            }
+
+            throw new UnableToProvideConfigurationObject<ManifestResourceStreamConfigurationObjectsProvider>(type);
+        }
+
+        string GetFilenameFor(Type type)
+        {
+            return $"{type.GetFriendlyConfigurationName()}.json";
         }
     }
 }
