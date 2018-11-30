31c134325de51b2d2500e469551d678ae8a3a64e
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-30 10:58:59 +0100

    Serializer does no longer need IContainer

--- Specifications/Serialization.Json/for_Serializer/given/a_serializer.cs
+++ Specifications/Serialization.Json/for_Serializer/given/a_serializer.cs
@@ -10,18 +10,16 @@ namespace Dolittle.Serialization.Json.Specs.for_Serializer.given
     public class a_serializer
     {
         protected static Serializer serializer;
-        protected static Mock<IContainer> container_mock;
         protected static Mock<IInstancesOf<ICanProvideConverters>> converter_provider_instances;
         protected static List<ICanProvideConverters> converter_providers;
 
         Establish context = () =>
                                 {
-                                    container_mock = new Mock<IContainer>();
                                     converter_providers = new List<ICanProvideConverters>();
                                     
                                     converter_provider_instances = new Mock<IInstancesOf<ICanProvideConverters>>();
                                     converter_provider_instances.Setup(c => c.GetEnumerator()).Returns(() => converter_providers.GetEnumerator());
-                                    serializer = new Serializer(container_mock.Object, converter_provider_instances.Object);
+                                    serializer = new Serializer(converter_provider_instances.Object);
                                 };
     }
 }
