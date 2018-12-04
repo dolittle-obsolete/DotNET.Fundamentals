3e26457b9e5e5563f4258f999789017d2f6ef1dd
Einar Ingebrigtsen <einar@dolittle.com>
2018-12-04 19:39:46 +0100

    Outputting which boot procedure is running

--- Source/Booting/Stages/08-BootProcedures/BootProcedures.cs
+++ Source/Booting/Stages/08-BootProcedures/BootProcedures.cs
@@ -2,7 +2,9 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using Dolittle.Collections;
 using Dolittle.Logging;
+using Dolittle.Types;
 
 namespace Dolittle.Booting.Stages
 {
@@ -20,6 +22,15 @@ namespace Dolittle.Booting.Stages
             if( settings.Enabled )
             {
                 var logger = builder.GetAssociation(WellKnownAssociations.Logger) as ILogger;
+                var typeFinder = builder.GetAssociation(WellKnownAssociations.TypeFinder) as ITypeFinder;
+                var procedures = typeFinder.FindMultiple<ICanPerformBootProcedure>();
+
+                procedures.ForEach(_ => logger.Information($"BootProcedure : {_.AssemblyQualifiedName}"));
+
+
+                
+
+                
                 Bootstrapper.Start(builder.Container, logger);
             }
         }
