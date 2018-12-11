db5b49d089f0c610397bf2cc2de5d4392d99c2df
Michael Smith <michael@dolittle.com>
2018-12-11 09:53:43 +0100

    Need to pass the ICanResolvePrincipal into the User extension for the Securable or else Security doesn’t work.  This is just to get it up and running.  We need an overhaul.

--- Specifications/Security/for_SecurableExtensions/when_specifying_user.cs
+++ Specifications/Security/for_SecurableExtensions/when_specifying_user.cs
@@ -11,10 +11,15 @@ namespace Dolittle.Security.Specs.for_SecurableExtensions
     {
         static TypeSecurable securable;
         static ISecurityActor actor;
+        static ICanResolvePrincipal principalResolver;
 
-        Establish context = () => securable = new TypeSecurable(typeof(object));
+        Establish context = () => 
+        {
+            securable = new TypeSecurable(typeof(object));
+            principalResolver = new Mock<ICanResolvePrincipal>().Object;
+        };
 
-        Because of = () => actor = securable.User();
+        Because of = () => actor = securable.UserFrom(principalResolver);
 
         It should_return_an_user_actor_builder = () => actor.ShouldBeOfExactType<UserSecurityActor>();
         It should_add_actor_to_securable = () => securable.Actors.First().ShouldBeOfExactType<UserSecurityActor>();
