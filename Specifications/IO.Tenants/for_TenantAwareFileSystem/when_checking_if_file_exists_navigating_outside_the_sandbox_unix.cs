4965e31573bf37fc305581040363d105bb5815a0
Einar Ingebrigtsen <einar@dolittle.com>
2019-02-14 18:42:49 +0100

    Adding more specs to be sure we're not going to get outside the sandbox

---
+++ Specifications/IO.Tenants/for_TenantAwareFileSystem/when_checking_if_file_exists_navigating_outside_the_sandbox_unix.cs
@@ -0,0 +1,17 @@
+using System;
+using Dolittle.Execution;
+using Machine.Specifications;
+using Moq;
+using It=Machine.Specifications.It;
+
+namespace Dolittle.IO.Tenants.for_TenantAwareFileSystem
+{
+    public class when_checking_if_file_exists_navigating_outside_the_sandbox_unix : given.a_tenant_aware_file_system
+    {
+        static Exception result;
+
+        Because of = () => result = Catch.Exception(() => tenant_aware_file_system.Exists("someplace/../../somefile.txt"));
+
+        It should_throw_access_outside_sandbox_denied = () => result.ShouldBeOfExactType<AccessOutsideSandboxDenied>();
+    }
+}
---
+++ Specifications/IO.Tenants/for_TenantAwareFileSystem/when_getting_directory_entries_navigating_outside_the_sandbox_windows.cs
@@ -0,0 +1,17 @@
+using System;
+using Dolittle.Execution;
+using Machine.Specifications;
+using Moq;
+using It=Machine.Specifications.It;
+
+namespace Dolittle.IO.Tenants.for_TenantAwareFileSystem
+{
+    public class when_getting_directory_entries_navigating_outside_the_sandbox_windows : given.a_tenant_aware_file_system
+    {
+        static Exception result;
+
+        Because of = () => result = Catch.Exception(() => tenant_aware_file_system.GetDirectoriesIn("someplace\\..\\..\\somefile.txt"));
+
+        It should_throw_access_outside_sandbox_denied = () => result.ShouldBeOfExactType<AccessOutsideSandboxDenied>();
+    }
+}
