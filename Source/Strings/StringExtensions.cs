77e60136a38fbc2b7e0e2cb448ab00982e14f147
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-30 19:52:03 +0200

    Adding color coding for strings

--- Source/Strings/StringExtensions.cs
+++ Source/Strings/StringExtensions.cs
@@ -11,40 +11,78 @@ namespace Dolittle.Strings
     /// </summary>
     public static class StringExtensions
     {
+        const string _colorPrepend = "\x1b[";
+        const string _redCode = "31m";
+        const string _yellowCode = "33m";
+        const string _whiteCode = "37m";
+        const string _resetCode = "0m";
+        
+
         /// <summary>
         /// Convert a string into a camel cased string
         /// </summary>
-        /// <param name="str">string to convert</param>
+        /// <param name="string">string to convert</param>
         /// <returns>Converted string</returns>
-        public static string ToCamelCase(this string str)
+        public static string ToCamelCase(this string @string)
         {
-            if (!string.IsNullOrEmpty(str))
+            if (!string.IsNullOrEmpty(@string))
             {
-                if (str.Length == 1)
-                    return str.ToLowerInvariant();
+                if (@string.Length == 1)
+                    return @string.ToLowerInvariant();
 
-                var firstLetter = str[0].ToString().ToLowerInvariant();
-                return firstLetter + str.Substring(1);
+                var firstLetter = @string[0].ToString().ToLowerInvariant();
+                return firstLetter + @string.Substring(1);
             }
-            return str;
+            return @string;
         }
 
         /// <summary>
         /// Convert a string into a pascal cased string
         /// </summary>
-        /// <param name="str">string to convert</param>
+        /// <param name="string">string to convert</param>
         /// <returns>Converted string</returns>
-        public static string ToPascalCase(this string str)
+        public static string ToPascalCase(this string @string)
         {
-            if (!string.IsNullOrEmpty(str))
+            if (!string.IsNullOrEmpty(@string))
             {
-                if (str.Length == 1)
-                    return str.ToUpperInvariant();
+                if (@string.Length == 1)
+                    return @string.ToUpperInvariant();
 
-                var firstLetter = str[0].ToString().ToUpperInvariant();
-                return firstLetter + str.Substring(1);
+                var firstLetter = @string[0].ToString().ToUpperInvariant();
+                return firstLetter + @string.Substring(1);
             }
-            return str;
+            return @string;
         }
+
+        /// <summary>
+        /// Prepend color coding for Red - typically used in CLI and such
+        /// </summary>
+        /// <param name="string">string to prepend to</param>
+        /// <returns>String with prepended color coding</returns>
+        public static string Red(this string @string)
+        {
+            return _colorPrepend + _redCode + @string + _colorPrepend + _resetCode;
+        }
+
+        /// <summary>
+        /// Prepend color coding for Yellow - typically used in CLI and such
+        /// </summary>
+        /// <param name="string">string to prepend to</param>
+        /// <returns>String with prepended color coding</returns>
+        public static string Yellow(this string @string)
+        {
+            return _colorPrepend + _yellowCode + @string + _colorPrepend + _resetCode;
+        }
+
+
+        /// <summary>
+        /// Prepend color coding for White - typically used in CLI and such
+        /// </summary>
+        /// <param name="string">string to prepend to</param>
+        /// <returns>String with prepended color coding</returns>
+        public static string White(this string @string)
+        {
+            return _colorPrepend + _whiteCode + @string + _colorPrepend + _resetCode;
+        }        
     }
 }
