9d04cdd04bba16d16bda72ba08ab9c9eb916eff0
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-30 19:53:01 +0200

    Adding failsafe and error messaging

--- Source/Build/Program.cs
+++ Source/Build/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Dolittle.Assemblies;
 using Dolittle.Booting;
 using Dolittle.Collections;
+using Dolittle.Strings;
 using Dolittle.Types;
 using Microsoft.Extensions.Logging.Abstractions;
 
@@ -15,30 +16,56 @@ namespace Dolittle.Build
 
     class Program
     {
+        internal static BuildConfiguration BuildConfiguration;
+
         static int Main(string[] args)
         {
-            Console.WriteLine("Performing Dolittle post-build steps");
+            try
+            {
+                var startTime = DateTime.UtcNow;
 
-            var assembly = args[0];
-            var pluginAssemblies = args[1].Split(";");
-            var configurationFile = args[2];
+                var assembly = args[0];
+                var pluginAssemblies = args[1].Split(";");
+                var configurationFile = args[2];
 
-            Console.WriteLine($"  Performing for: {assembly}");
-            Console.WriteLine("  Using plugins from: ");
+                if (string.IsNullOrEmpty(args[1]) ||
+                    pluginAssemblies.Length == 0 ||
+                    string.IsNullOrEmpty(configurationFile)) return 0;
 
-            foreach (var pluginAssembly in pluginAssemblies)
-                Console.WriteLine($"    {pluginAssembly}");
+                BuildConfiguration = new BuildConfiguration(assembly);
 
-            var bootLoaderResult = Bootloader.Configure(_ => _
-                .WithAssemblyProvider(new AssemblyProvider(new Dolittle.Logging.NullLogger(),pluginAssemblies))
-                .NoLogging()
-                .SkipBootprocedures()
-            ).Start();
+                Console.WriteLine("Performing Dolittle post-build steps");
 
-            var configuration = bootLoaderResult.Container.Get<IPerformerConfigurationManager>();
-            configuration.Initialize(configurationFile);
-            var performers = bootLoaderResult.Container.Get<IPostBuildTaskPerformers>();
-            performers.Perform();
+                Console.WriteLine($"  Performing for: {assembly}");
+                Console.WriteLine("  Using plugins from: ");
+
+                foreach (var pluginAssembly in pluginAssemblies)
+                    Console.WriteLine($"    {pluginAssembly}");
+
+                var bootLoaderResult = Bootloader.Configure(_ => _
+                    .WithAssemblyProvider(new AssemblyProvider(new Dolittle.Logging.NullLogger(), pluginAssemblies))
+                    .NoLogging()
+                    .SkipBootprocedures()
+                ).Start();
+
+                var buildMessages = bootLoaderResult.Container.Get<IBuildMessages>();
+
+                var configuration = bootLoaderResult.Container.Get<IPerformerConfigurationManager>();
+                configuration.Initialize(configurationFile);
+                var performers = bootLoaderResult.Container.Get<IPostBuildTaskPerformers>();
+                performers.Perform();
+
+                var endTime = DateTime.UtcNow;
+                var deltaTime = endTime.Subtract(startTime);
+                buildMessages.Information($"Time Elapsed {deltaTime.ToString("G")} (Dolittle)");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error executing Dolittle post build tool".Red());
+                Console.Error.WriteLine($"Exception: {ex.Message}".Red());
+                Console.Error.WriteLine($"StackTrace: {ex.StackTrace}".Red());
+                return 1;
+            }
 
             return 0;
         }
