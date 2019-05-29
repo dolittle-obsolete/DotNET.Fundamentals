979c606200855153ac2ef1363f1fe03adf4ce2e4
Michael Smith <michael@dolittle.com>
2019-05-29 14:30:25 +0200

    Remove unneccessary ctor overrides and add summary text

--- Source/Serialization.Json/UnableToInstantiateInstanceOfType.cs
+++ Source/Serialization.Json/UnableToInstantiateInstanceOfType.cs
@@ -4,7 +4,8 @@ namespace Dolittle.Serialization.Json
     using System.Runtime.Serialization;
     
     /// <summary>
-    /// 
+    /// Indicates when the serializer is unable to instantiate a type.
+    /// Typically due to the lack of a default constructor and mismatched parameters in the non-default constructor(s).
     /// </summary>
     [Serializable]
     public class UnableToInstantiateInstanceOfType : Exception
@@ -13,33 +14,7 @@ namespace Dolittle.Serialization.Json
         ///     Initializes a new instance of the UnableToInstantiateInstanceOfType custom exception
         /// </summary>
         public UnableToInstantiateInstanceOfType(Type type) 
-            : this($"Cannot instantiate type {type?.FullName ?? "[NULL]"}.  Ensure that the type has a default constructor or that there is a matching constructor with the ctor parameters matching the property names.")
-        {}
-    
-        /// <summary>
-        ///     Initializes a new instance of the UnableToInstantiateInstanceOfType custom exception
-        /// </summary>
-        /// <param name="message">A message describing the exception</param>
-        public UnableToInstantiateInstanceOfType(string message)
-            : base(message)
-        {}
-    
-        /// <summary>
-        ///     Initializes a new instance of the UnableToInstantiateInstanceOfType custom exception
-        /// </summary>
-        /// <param name="message">A message describing the exception</param>
-        /// <param name="innerException">An inner exception that is the original source of the error</param>
-        public UnableToInstantiateInstanceOfType(string message, Exception innerException)
-            : base(message, innerException)
-        {}
-    
-        /// <summary>
-        ///     Initializes a new instance of the UnableToInstantiateInstanceOfType custom exception
-        /// </summary>
-        /// <param name="info">The SerializationInfo that holds the object data of the exception</param>
-        /// <param name="context">The StreamingContext that contains contextual information about the source or destination</param>
-        protected UnableToInstantiateInstanceOfType(SerializationInfo info, StreamingContext context)
-            : base(info, context)
+            : base($"Cannot instantiate type {type?.FullName ?? "[NULL]"}.  Ensure that the type has a default constructor or that there is a matching constructor with the ctor parameters matching the property names.")
         {}
     }
 }
