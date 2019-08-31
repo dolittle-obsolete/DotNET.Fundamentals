6f78297249c2b625290d107a2a358d380255803a
Einar Ingebrigtsen <einar@dolittle.com>
2019-08-31 09:04:50 +0200

    Grpc Hosting up and running

---
+++ Source/Grpc/IHosts.cs
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+
+namespace Dolittle.Grpc
+{
+    /// <summary>
+    /// Defines a system that manages all the <see cref="IHost">hosts</see>
+    /// </summary>
+    public interface IHosts : IDisposable
+    {
+        /// <summary>
+        /// Start all the hosts
+        /// </summary>
+        void Start();
+    }
+}
---
+++ Specifications/Grpc/for_Hosts/given/ICanBindMyHostType.cs
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+namespace Dolittle.Grpc.given
+{
+    public interface ICanBindMyHostType : ICanBindServices 
+    {
+        
+    }
+}
