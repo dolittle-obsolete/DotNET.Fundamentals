843fa3a5e38784f9c647571fe95325f4aa3f555f
Einar Ingebrigtsen <einar@dolittle.com>
2019-09-12 09:43:31 +0200

    Adding concept conversion + fixing NS

--- Source/Protobuf/Extensions.cs
+++ Source/Protobuf/Extensions.cs
@@ -4,14 +4,26 @@
  *--------------------------------------------------------------------------------------------*/
 using Grpc.Core;
 using Google.Protobuf;
+using Dolittle.Concepts;
 
-namespace System.Protobuf
+namespace Dolittle.Protobuf
 {
     /// <summary>
     /// Represents conversion extensions for the common types
     /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Convert a <see cref="System.Protobuf.guid"/> to a <see cref="ConceptAs{T}"/> of type <see cref="System.Guid"/>
+        /// </summary>
+        /// <param name="guid"><see cref="System.Protobuf.guid"/> to convert</param>
+        /// <returns>Converted <see cref="ConceptAs{T}"/> of type <see cref="System.Guid"/></returns>
+        public static System.Guid ToGuidOf<T>(this System.Protobuf.guid guid) where T:ConceptAs<Guid>, new()
+        {
+            return new T { Value = new System.Guid(guid.Value.ToByteArray()) };
+        }
+
+
         /// <summary>
         /// Convert a <see cref="System.Protobuf.guid"/> to <see cref="System.Guid"/>
         /// </summary>
@@ -27,11 +39,23 @@ namespace System.Protobuf
         /// </summary>
         /// <param name="guid"><see cref="System.Guid"/> to convert</param>
         /// <returns>Converted <see cref="System.Protobuf.guid"/></returns>
-        public static System.Protobuf.guid ToGuid(this System.Guid guid)
+        public static System.Protobuf.guid ToProtobuf(this System.Guid guid)
         {
             var converted = new System.Protobuf.guid();
             converted.Value = ByteString.CopyFrom(guid.ToByteArray());
             return converted;
         }
+
+        /// <summary>
+        /// Convert a <see cref="ConceptAs{T}"/> of type <see cref="System.Guid"/> to <see cref="System.Protobuf.guid"/>
+        /// </summary>
+        /// <param name="guid"><see cref="ConceptAs{T}"/> of type <see cref="System.Guid"/> to convert</param>
+        /// <returns>Converted <see cref="System.Protobuf.guid"/></returns>
+        public static System.Protobuf.guid ToProtobuf(this ConceptAs<System.Guid> guid)
+        {
+            var converted = new System.Protobuf.guid();
+            converted.Value = ByteString.CopyFrom(guid.Value.ToByteArray());
+            return converted;
+        }        
     }
 }
