a91c363441693d89e2c040f83bafe05001ca353c
Einar Ingebrigtsen <einar@dolittle.com>
2019-10-02 18:51:42 +0200

    Moving actual System protobufs out to Contracts

--- Source/Protobuf/Extensions.cs
+++ Source/Protobuf/Extensions.cs
@@ -2,7 +2,6 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-using Grpc.Core;
 using Google.Protobuf;
 using Dolittle.Concepts;
 
