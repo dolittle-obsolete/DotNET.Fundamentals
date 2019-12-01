8d1f90377333578134929773832da943349285c7
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-01 09:44:38 +0100

    Static code analysis fixes

--- Source/DependencyInversion.Management/BindingCollection.cs
+++ Source/DependencyInversion.Management/BindingCollection.cs
@@ -1,8 +1,8 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 extern alias management;
+
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +11,16 @@ using grpc = management::Dolittle.DependencyInversion.Management;
 namespace Dolittle.DependencyInversion.Management
 {
     /// <summary>
-    /// Represents all <see cref="Binding">bindings</see> in the system
+    /// Represents all <see cref="Binding">bindings</see> in the system.
     /// </summary>
     public class BindingCollection : IEnumerable<grpc.Binding>
     {
         readonly List<grpc.Binding> _bindings;
 
         /// <summary>
-        /// Initializes a new instance of <see cref="BindingCollection"/>
+        /// Initializes a new instance of the <see cref="BindingCollection"/> class.
         /// </summary>
-        /// <param name="bindings">Collection of all <see cref="Binding">bindings</see></param>
+        /// <param name="bindings">Collection of all <see cref="Binding">bindings</see>.</param>
         public BindingCollection(IEnumerable<DependencyInversion.Binding> bindings) => _bindings = new List<grpc.Binding>(bindings.Select(_ => _.ToProtobuf()));
 
         /// <inheritdoc/>
@@ -29,6 +29,7 @@ namespace Dolittle.DependencyInversion.Management
             return _bindings.GetEnumerator();
         }
 
+        /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return _bindings.GetEnumerator();
