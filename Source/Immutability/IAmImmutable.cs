1eddcd69b20f55c78041293d4d94b7164d78c668
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-02 08:02:09 +0100

    Static code analysis fixes

--- Source/Immutability/IAmImmutable.cs
+++ Source/Immutability/IAmImmutable.cs
@@ -1,10 +1,12 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace Dolittle.Immutability
 {
     /// <summary>
-    /// A marker interface to indicate that an object is immutable
+    /// A marker interface to indicate that an object is immutable.
     /// </summary>
     public interface IAmImmutable
     {
-         
     }
 }
