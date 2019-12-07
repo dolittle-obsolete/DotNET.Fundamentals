1ebe459069d6519398058746b1ee03078d1a360a
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-07 03:04:18 -0800

    Static code analysis fixes

--- Source/Configuration/Booting/Stages/06-Configuration/Configuration.cs
+++ Source/Configuration/Booting/Stages/06-Configuration/Configuration.cs
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Dolittle.Booting;
 using Dolittle.Collections;
 using Dolittle.Immutability;
@@ -11,34 +10,35 @@ using Dolittle.Types;
 namespace Dolittle.Configuration
 {
     /// <summary>
-    /// Represents the system that manages the bindings for the IoC container for the 
-    /// <see cref="IConfigurationObject">configuration objects</see> in the system
+    /// Represents the system that manages the bindings for the IoC container for the
+    /// <see cref="IConfigurationObject">configuration objects</see> in the system.
     /// </summary>
     public class Configuration : ICanPerformBootStage<NoSettings>
     {
-         /// <inheritdoc/>
+        /// <inheritdoc/>
         public BootStage BootStage => BootStage.Configuration;
 
-         /// <inheritdoc/>
+        /// <inheritdoc/>
         public void Perform(NoSettings settings, IBootStageBuilder builder)
         {
             var typeFinder = builder.GetAssociation(WellKnownAssociations.TypeFinder) as ITypeFinder;
             var logger = builder.GetAssociation(WellKnownAssociations.Logger) as ILogger;
-            
+
             var configurationObjectProviders = new ConfigurationObjectProviders(typeFinder, builder.Container, logger);
             builder.Bindings.Bind<IConfigurationObjectProviders>().To(configurationObjectProviders);
 
             var configurationObjectTypes = typeFinder.FindMultiple<IConfigurationObject>();
-            configurationObjectTypes.ForEach((System.Action<System.Type>)(_ => 
+            configurationObjectTypes.ForEach(_ =>
             {
-                logger.Trace((string)$"Bind configuration object '{_.GetFriendlyConfigurationName()} - {_.AssemblyQualifiedName}'");
+                logger.Trace($"Bind configuration object '{_.GetFriendlyConfigurationName()} - {_.AssemblyQualifiedName}'");
                 _.ShouldBeImmutable();
-                builder.Bindings.Bind(_).To((System.Func<object>)(() => {
+                builder.Bindings.Bind(_).To(() =>
+                {
                     var instance = configurationObjectProviders.Provide(_);
-                    logger.Trace((string)$"Providing configuration object '{_.GetFriendlyConfigurationName()} - {_.AssemblyQualifiedName}' - {instance.GetHashCode()}");
+                    logger.Trace($"Providing configuration object '{_.GetFriendlyConfigurationName()} - {_.AssemblyQualifiedName}' - {instance.GetHashCode()}");
                     return instance;
-                }));
-            }));
+                });
+            });
         }
-    }   
+    }
 }
--- Source/Configuration/IConfigurationObjectProviders.cs
+++ Source/Configuration/IConfigurationObjectProviders.cs
@@ -1,23 +1,22 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 
 namespace Dolittle.Configuration
 {
     /// <summary>
-    /// Represents all the providers for <see cref="IConfigurationObject"/>
+    /// Represents all the providers for <see cref="IConfigurationObject"/>.
     /// </summary>
     public interface IConfigurationObjectProviders
     {
         /// <summary>
-        /// Provide an instance of <see cref="IConfigurationObject"/>
+        /// Provide an instance of <see cref="IConfigurationObject"/>.
         /// </summary>
-        /// <param name="type"><see cref="Type"/> of <see cref="IConfigurationObject"/></param>
-        /// <returns>An instance if provided - throws an exception if it can't provide it</returns>
-        /// <exception cref="MultipleProvidersProvidingConfigurationObject">Thrown when there are multiple providers providing <see cref="IConfigurationObject"/></exception>
-        /// <exception cref="MissingProviderForConfigurationObject">Thrown when there are no providers capable of providing a given <see cref="IConfigurationObject"/></exception>
+        /// <param name="type"><see cref="Type"/> of <see cref="IConfigurationObject"/>.</param>
+        /// <returns>An instance if provided - throws an exception if it can't provide it.</returns>
+        /// <exception cref="MultipleProvidersProvidingConfigurationObject">Thrown when there are multiple providers providing <see cref="IConfigurationObject"/>.</exception>
+        /// <exception cref="MissingProviderForConfigurationObject">Thrown when there are no providers capable of providing a given <see cref="IConfigurationObject"/>.</exception>
         object Provide(Type type);
     }
 }
