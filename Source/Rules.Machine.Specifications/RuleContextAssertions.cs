1e46dfe0d6f370921b9ad6f7137b0ef3e3791e85
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-07 02:10:15 -0800

    Static code analysis fixes

--- Source/Rules.Machine.Specifications/RuleContextAssertions.cs
+++ Source/Rules.Machine.Specifications/RuleContextAssertions.cs
@@ -1,21 +1,20 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Moq;
 
 namespace Dolittle.Rules.Machine.Specifications
 {
     /// <summary>
-    /// Represents assertions related to <see cref="IRuleContext"/>
+    /// Represents assertions related to <see cref="IRuleContext"/>.
     /// </summary>
     public static class RuleContextAssertions
     {
         /// <summary>
         /// Asserts that a <see cref="Mock{T}"/> of <see cref="IRuleContext"/> never has any failures
-        /// added to it
+        /// added to it.
         /// </summary>
-        /// <param name="ruleContextMock"><see cref="Mock{T}"/> of <see cref="IRuleContext"/></param>
+        /// <param name="ruleContextMock"><see cref="Mock{T}"/> of <see cref="IRuleContext"/>.</param>
         public static void ShouldNotFail(this Mock<IRuleContext> ruleContextMock)
         {
             ruleContextMock.Verify(r => r.Fail(Moq.It.IsAny<IRule>(), Moq.It.IsAny<object>(), Moq.It.IsAny<Cause>()), Times.Never());
@@ -23,12 +22,12 @@ namespace Dolittle.Rules.Machine.Specifications
 
         /// <summary>
         /// Asserts that a <see cref="Mock{T}"/> of <see cref="IRuleContext"/> fails with specific
-        /// <see cref="IRule"/>, instance and <see cref="Reason"/>
+        /// <see cref="IRule"/>, instance and <see cref="Reason"/>.
         /// </summary>
-        /// <param name="ruleContextMock"></param>
-        /// <param name="rule"></param>
-        /// <param name="instance"></param>
-        /// <param name="reason"></param>
+        /// <param name="ruleContextMock"><see cref="Mock{T}"/> of <see cref="IRuleContext"/>.</param>
+        /// <param name="rule"><see cref="IRule"/> it should fail with.</param>
+        /// <param name="instance">Instance it should fail with.</param>
+        /// <param name="reason"><see cref="Reason"/> it should fail with.</param>
         public static void ShouldFailWith(this Mock<IRuleContext> ruleContextMock, IRule rule, object instance, Reason reason)
         {
             ruleContextMock.Verify(r => r.Fail(rule, instance, Moq.It.Is<Cause>(_ => _.Reason == reason)), Times.Once());
