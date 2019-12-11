668eb2fef387bd6e781822406094e48f340f7374
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-11 19:32:08 +0100

    Static code analysis fixes

--- Specifications/ResourceTypes.Configuration/given/all_dependencies.cs
+++ Specifications/ResourceTypes.Configuration/given/all_dependencies.cs
@@ -1,19 +1,17 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
- 
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Dolittle.Tenancy;
 
 namespace Dolittle.ResourceTypes.Configuration.Specs.given
 {
     public class all_dependencies
     {
-        protected static readonly TenantId tenant_id = TenantId.System; 
         public static readonly ResourceType first_resource_type = "first_resource_type";
         public static readonly ResourceType second_resource_type = "second_resource_type";
         public static readonly ResourceType third_resource_type = "third_resource_type";
         public static readonly ResourceTypeImplementation first_resource_type_implementation = "first_resource_type_implementation";
         public static readonly ResourceTypeImplementation second_resource_type_implementation = "second_resource_type_implementation";
+        protected static readonly TenantId tenant_id = TenantId.System;
     }
 }
--- Specifications/ResourceTypes.Configuration/given/third_service.cs
+++ Specifications/ResourceTypes.Configuration/given/third_service.cs
@@ -1,15 +1,11 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
- 
-using System;
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
 
 namespace Dolittle.ResourceTypes.Configuration.Specs.given
 {
     public interface third_service
     {
-        
     }
 }
