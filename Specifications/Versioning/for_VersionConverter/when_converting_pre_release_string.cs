e9b318126be4d2c36ec872548d66e93b62df1100
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-13 15:19:35 +0100

    Fixing specs to be correct

--- Specifications/Versioning/for_VersionConverter/when_converting_pre_release_string.cs
+++ Specifications/Versioning/for_VersionConverter/when_converting_pre_release_string.cs
@@ -15,7 +15,7 @@ namespace Dolittle.Versioning.for_VersionConverter
         It should_hold_correct_minor = () => version.Minor.ShouldEqual(2);
         It should_hold_correct_patch = () => version.Patch.ShouldEqual(3);
         It should_hold_correct_build = () => version.Build.ShouldEqual(4);
-        It should_be_considered_as_release_build = () => version.IsPreRelease.ShouldBeFalse();
+        It should_be_considered_as_release_build = () => version.IsPreRelease.ShouldBeTrue();
         It should_not_hold_a_pre_release_string = () => version.PreReleaseString.ShouldEqual("alpha1");
     }
 }
