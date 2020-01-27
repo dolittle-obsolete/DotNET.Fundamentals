e783ad7b086b665e6d5e94bdffad8ee9667e406a
Einar Ingebrigtsen <einar@dolittle.com>
2020-01-27 13:22:09 +0100

    Adding stream sample

--- Samples/Services/Client/Program.cs
+++ Samples/Services/Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Threading;
 using System.Threading.Tasks;
 using Contracts;
 using Dolittle.Booting;
@@ -31,6 +32,24 @@ namespace Client
             var client = result.Container.Get<TestServiceClient>();
             var response = await client.SayHelloToAsync(new Request { Name = "Yoda" });
             logger.Information($"Response was : {response.Message}");
+
+            var counter = 0;
+            var stream = client.SayHelloToStream()
+            var timer = new System.Timers.Timer(1000);
+            timer.Elapsed += (s, e) => stream.RequestStream.WriteAsync(new Request { Name = $"R2-D{counter++}" });
+            timer.Start();
+
+            var task = Task.Run(async () =>
+            {
+                while (await stream.ResponseStream.MoveNext(CancellationToken.None).ConfigureAwait(false))
+                {
+                    logger.Information($"Response : {stream.ResponseStream.Current.Message}");
+                }
+            });
+
+            await host.RunAsync().ConfigureAwait(false);
+
+            timer.Dispose();
 
             loggerFactory.Dispose();
         }
