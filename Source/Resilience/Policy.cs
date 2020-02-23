a356e136d68ebc7cd675890b0a16ab9166c9239e
Sindre Wilting <sindrewilting@gmail.com>
2020-02-23 13:19:42 +0100

    Throw exception when executing synchronous policy on async action

--- Source/Resilience/Policy.cs
+++ Source/Resilience/Policy.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Threading.Tasks;
 
 namespace Dolittle.Resilience
 {
@@ -48,8 +49,14 @@ namespace Dolittle.Resilience
         /// <inheritdoc/>
         public TResult Execute<TResult>(Func<TResult> action)
         {
+            ThrowIfAsyncAction(typeof(TResult));
             if (DelegatedPolicy != null) return DelegatedPolicy.Execute(action);
             return UnderlyingPolicy.Execute(action);
+        }
+
+        void ThrowIfAsyncAction(Type type)
+        {
+            if (typeof(Task).IsAssignableFrom(type)) throw new SynchronousPolicyCannotReturnTask(type);
         }
     }
 }
