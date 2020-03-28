1a6daf1344e961c6795de3552339cd0d49c9532c
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-03-28 11:30:11 +0100

    Update Logger interfaces.
    They are now more in line with popular frameworks.
    Uses typed loggers to indicate what type is logging (not file).
    Uses formatted messages with arguments.
    
    This breaks the interface - but not really afterall.

--- Source/Logging/Logger.cs
+++ Source/Logging/Logger.cs
@@ -2,75 +2,84 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.Lifecycle;
 
 namespace Dolittle.Logging
 {
     /// <summary>
     /// Represents an implementation of <see cref="ILogger"/>.
     /// </summary>
-    [Singleton]
-    public class Logger : ILogger
+    internal class Logger : ILogger
     {
-        readonly ILogAppenders _logAppenders;
-
-        static Logger() => Internal = new NullLogger();
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Logger"/> class.
-        /// </summary>
-        /// <param name="logAppenders">The <see cref="ILogAppenders">log appenders</see>.</param>
-        public Logger(ILogAppenders logAppenders)
-        {
-            _logAppenders = logAppenders;
-            Internal = this;
-        }
-
-        /// <summary>
-        /// Gets the internal logger for those scenarios where it can't be or it is inconvenient to get it injected.
-        /// </summary>
-        public static ILogger Internal { get; private set; }
+        public ILogMessageWriter[] LogMessageWriters { get; set; }
 
         /// <inheritdoc/>
-        public void Trace(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Trace, message);
-        }
+        public void Trace(string message, params object[] args) => Write(LogLevel.Trace, message, args);
 
         /// <inheritdoc/>
-        public void Debug(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Debug, message);
-        }
+        public void Trace(Exception exception, string message, params object[] args) => Write(LogLevel.Trace, exception, message, args);
 
         /// <inheritdoc/>
-        public void Information(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Info, message);
-        }
+        public void Debug(string message, params object[] args) => Write(LogLevel.Debug, message, args);
 
         /// <inheritdoc/>
-        public void Warning(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Warning, message);
-        }
+        public void Debug(Exception exception, string message, params object[] args) => Write(LogLevel.Debug, exception, message, args);
 
         /// <inheritdoc/>
-        public void Critical(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Critical, message);
-        }
+        public void Information(string message, params object[] args) => Write(LogLevel.Info, message, args);
 
         /// <inheritdoc/>
-        public void Error(string message, string filePath, int lineNumber, string member)
-        {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Error, message);
-        }
+        public void Information(Exception exception, string message, params object[] args) => Write(LogLevel.Info, exception, message, args);
 
         /// <inheritdoc/>
-        public void Error(Exception exception, string message, string filePath, int lineNumber, string member)
+        public void Warning(string message, params object[] args) => Write(LogLevel.Warning, message, args);
+
+        /// <inheritdoc/>
+        public void Warning(Exception exception, string message, params object[] args) => Write(LogLevel.Warning, exception, message, args);
+
+        /// <inheritdoc/>
+        public void Critical(string message, params object[] args) => Write(LogLevel.Critical, message, args);
+
+        /// <inheritdoc/>
+        public void Critical(Exception exception, string message, params object[] args) => Write(LogLevel.Critical, exception, message, args);
+
+        /// <inheritdoc/>
+        public void Error(string message, params object[] args) => Write(LogLevel.Error, message, args);
+
+        /// <inheritdoc/>
+        public void Error(Exception exception, string message, params object[] args) => Write(LogLevel.Error, exception, message, args);
+
+        void Write(LogLevel logLevel, string message, params object[] args)
         {
-            _logAppenders.Append(filePath, lineNumber, member, LogLevel.Error, message, exception);
+            var writers = LogMessageWriters;
+            if (writers == null) return;
+
+            for (var i = 0; i < writers.Length; ++i)
+            {
+                try
+                {
+                    writers[i].Write(logLevel, message, args);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        void Write(LogLevel logLevel, Exception exception, string message, params object[] args)
+        {
+            var writers = LogMessageWriters;
+            if (writers == null) return;
+
+            for (var i = 0; i < writers.Length; ++i)
+            {
+                try
+                {
+                    writers[i].Write(logLevel, exception, message, args);
+                }
+                catch
+                {
+                }
+            }
         }
     }
 }
--- Source/Logging/NullLogger.cs
+++ Source/Logging/NullLogger.cs
@@ -6,42 +6,73 @@ using System;
 namespace Dolittle.Logging
 {
     /// <summary>
-    /// Represents a null implementation of <see cref="ILogger"/> - it does not do anything.
+    /// Represents an implementation of <see cref="ILogger"/> that does nothing.
     /// </summary>
-    public class NullLogger : ILogger
+    /// <typeparam name="T">The type that the log messages relate to.</typeparam>
+    internal class NullLogger<T> : ILogger<T>
     {
+        /// <summary>
+        /// Gets the static singleton instance of <see cref="NullLogger{T}"/>.
+        /// </summary>
+        public static ILogger<T> Instance { get; } = new NullLogger<T>();
+
         /// <inheritdoc/>
-        public void Trace(string message, string filePath, int lineNumber, string member)
+        public void Trace(string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Debug(string message, string filePath, int lineNumber, string member)
+        public void Trace(Exception exception, string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Information(string message, string filePath, int lineNumber, string member)
+        public void Debug(string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Warning(string message, string filePath, int lineNumber, string member)
+        public void Debug(Exception exception, string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Critical(string message, string filePath, int lineNumber, string member)
+        public void Information(string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Error(string message, string filePath, int lineNumber, string member)
+        public void Information(Exception exception, string message, params object[] args)
         {
         }
 
         /// <inheritdoc/>
-        public void Error(Exception exception, string message, string filePath, int lineNumber, string member)
+        public void Warning(string message, params object[] args)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Warning(Exception exception, string message, params object[] args)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Critical(string message, params object[] args)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Critical(Exception exception, string message, params object[] args)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Error(string message, params object[] args)
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Error(Exception exception, string message, params object[] args)
         {
         }
     }
